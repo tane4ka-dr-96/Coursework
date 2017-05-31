40e02b70bfc81d89710331e97b6fd20ed0a5b9de
tane4ka-dr-96 <tane4ka-dr-96@yandex.ru>
2017-05-31 19:23:28 +0900

    Add files via upload

---
+++ Calculator/src/calculator/VariablesExtractorListener.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import java.util.ArrayList;
+
+public class VariablesExtractorListener extends LogicalCalculatorBaseListener {
+    
+    private ArrayList<String> variables = new ArrayList<String>();
+
+    @Override
+    public void exitVar(LogicalCalculatorParser.VarContext ctx) {
+        System.out.println("calculator.VariablesExtractorListener.exitVar()");
+        variables.add(ctx.getText());
+    }
+    
+    public ArrayList<String> getVariables(){
+        System.out.println(variables);
+        return variables;
+    }
+    
+    public Object[] getArray(){
+        return variables.toArray(new String[0]);   
+    }
+    
+}
